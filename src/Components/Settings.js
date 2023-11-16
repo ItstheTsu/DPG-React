c14ca842047d1b92dd061e060d896ad1c819dbf2
Tsu <allan.correagamex@gmail.com>
2023-11-16 10:17:13 -0300

    Att

---
+++ src/Components/Settings.js
@@ -0,0 +1,11 @@
+import styles from './Styles/Settings Style/Settings.module.css';
+
+function Settings(props) {
+    return (
+        <section className={styles.Settings} id={props.id}>
+
+        </section>
+    )
+}
+
+export default Settings;
